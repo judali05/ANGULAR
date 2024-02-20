fae3063691613c26f50b83226917faaa51d03297
Julian Rojas <129390687+judali05@users.noreply.github.com>
2024-02-20 09:36:28 -0500

    Add files via upload

---
+++ SENA/TALLER/A-1_Directivas/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  nombre = 'Julian';
+  edad = 17;
+
+  articulos = [{
+    codigo: 1,
+    descripcion: 'naranjas',
+    precio: 540
+  },{
+    codigo: 2,
+    descripcion: 'manzanas',
+    precio: 900
+  },{
+    codigo: 3,
+    descripcion: 'peras',
+    precio: 490
+  }];
+
+  generarNumero() {
+    return Math.floor(Math.random() * 3) + 1;
+  }
+
+}
